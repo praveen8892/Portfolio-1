092cd5d532f2fe170580ffb97cae413f93333967
Praveen <praveen.if@geopageconsultants.in>
2025-12-15 15:26:55 +0530

    skils

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import ThemeToggleButton from './ThemeToggleButton';
 const Header = () => {
   return (
     <header className="p-4">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center sticky top-0 z-10 bg-white dark:bg-gray-900">
         <h1 className="text-2xl font-bold">Portfolio</h1>
         <ThemeToggleButton />
       </div>
